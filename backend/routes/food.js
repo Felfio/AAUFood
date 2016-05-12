184a177d4218408b418ebcd9248431fa7e0f6011
Fabian <schoberf@gmx.net>
2016-05-13 01:52:07 +0200

    Mittagstisch starters and mains

--- backend/routes/food.js
+++ backend/routes/food.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 const scraper = require('../scraping/scraper');
 
-/* GET users listing. */
 router.get('/uniwirt', function(req, res, next) {
     scraper.downloadUniwirt()
+        .then(body => res.send(body));
+});
+
+router.get('/mittagstisch', function(req, res, next) {
+    scraper.downloadMittagstisch()
         .then(body => res.send(body));
 });
 
--- backend/scraping/scraper.js
+++ backend/scraping/scraper.js
@@ -1,6 +1,8 @@
 const Promise = require('bluebird');
 const request = Promise.promisifyAll(require("request"));
 const cheerio = require('cheerio');
+const Food = require("../models/food");
+const Menu = require("../models/menu");
 
 function downloadUniwirt() {
     return request.getAsync('http://www.uniwirt.at/')
@@ -18,6 +20,57 @@ function parseUniwirt(body) {
 
 }
 
+function downloadMittagstisch() {
+    return request.getAsync('http://www.lakeside-scitec.com/services/gastronomie/mittagstisch/')
+        .then(res => res.body)
+        .then(parseMittagstisch);
+}
+
+function parseMittagstisch(body) {
+    var foodMenu = new Menu();
+
+    var $ = cheerio.load(body);
+    var dayInWeek = (new Date().getDay() + 6) % 7;
+    dayInWeek = dayInWeek > 4 ? 0 : dayInWeek;
+    var plans = $(".resp-tabs-container .daydata");
+
+    //Menü
+    var menu = plans.eq(dayInWeek * 2);
+    var menuRows = menu.find("tr");
+    var first = menuRows.first();
+
+    var soupRows;
+    if (first.hasClass("free")) {
+        soupRows = first.nextUntil('.free', 'tr');
+    } else {
+        soupRows = first.nextUntil('.free', 'tr');
+        soupRows.unshift(first);
+    }
+
+    soupRows.each((index, item) => {
+        if (index != 0) {
+            var name = $(item).children().first().text();
+            foodMenu.starters.push(new Food(name));
+        }
+    });
+
+    //Hauptspeisen
+    var mainsRows = soupRows.last().next().nextUntil('.free', 'tr');
+
+    mainsRows.each((index, item) => {
+        if (index != 0) {
+            var name = $(item).children().first().text();
+            foodMenu.mains.push(new Food(name, "7.40 / 7.90"));
+        }
+    });
+
+    //á la carte
+    //Yet todo :)
+
+    return foodMenu;
+}
+
 module.exports = {
-    downloadUniwirt: downloadUniwirt
+    downloadUniwirt: downloadUniwirt,
+    downloadMittagstisch: downloadMittagstisch
 };
