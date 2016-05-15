7eb2885652913c30cb1febf15a0539e7c2c1b452
Fabian <schoberf@gmx.net>
2016-05-15 12:51:01 +0200

    Load models

--- frontend/app/routes/index.js
+++ frontend/app/routes/index.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  ajax: Ember.inject.service(),
+
+  model() {
+    return Ember.RSVP.hash({
+      mensa: this.get('ajax').request('/mensa'),
+      uniwirt: this.get('ajax').request('/uniwirt'),
+      mittagstisch: this.get('ajax').request('/mittagstisch'),
+    });
+  },
+
+  setupController(controller, model) {
+    this._super(...arguments);
+    Ember.set(controller, 'mensa', model.mensa);
+    Ember.set(controller, 'uniwirt', model.uniwirt);
+    Ember.set(controller, 'mittagstisch', model.mittagstisch);
+  }
 });
+
