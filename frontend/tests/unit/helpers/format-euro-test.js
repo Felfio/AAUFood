d3809ff07c200a4e4b70e8b3db831328129fabc4
Fabian <schoberf@gmx.net>
2016-05-20 22:15:58 +0200

    Format prices

---
+++ frontend/tests/unit/helpers/format-euro-test.js
@@ -0,0 +1,10 @@
+import { formatEuro } from 'aau-food-frontend/helpers/format-euro';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | format euro');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = formatEuro([42]);
+  assert.ok(result);
+});
