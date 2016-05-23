a66004696bb1eb2d704ce1120c4bef79c7275e77
Markus Sussitz <markus.sussitz@gmail.com>
2016-05-23 23:15:49 +0200

    Ich mag nicht mehr!

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const scraper = require('../scraping/scraper');
+const Promise = require("bluebird");
 
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+
+router.get('/', function (req, res, next) {
+    var uniwirtPlan = scraper.getUniwirtPlan();
+    var mensaPlan = scraper.getMensaPlan();
+    var mittagstischPlan = scraper.getMensaPlan();
+
+    Promise.all([uniwirtPlan, mensaPlan, mittagstischPlan])
+        .then(results => {
+            res.render('index', {uniwirt: results[0], mensa: results[1], mittagstisch: results[2]});
+        });
 });
 
 module.exports = router;
