0c4d84646b308d61cfcea11a8171644aadb5e70c
Fabian Schober <fabian@imendo.at>
2016-06-22 22:41:55 +0200

    Display week + swiping (working on it!)

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.set('view engine', 'ejs');
 
 app.use(compression());
 app.use(express.static(__dirname + '/public'));
+app.use("/modules", express.static(__dirname + "/node_modules"));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
