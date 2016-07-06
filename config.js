449b6601bc17bca9401a7dd508c4e703170922d8
Fabian <schoberf@gmx.net>
2016-07-07 01:23:30 +0200

    One more pun!

--- config.js
+++ config.js
@@ -26,6 +26,7 @@ module.exports = {
     footerPuns: [
         new FooterPun("heart", "Crafted with", "fa-heart"),
         new FooterPun("empire", "Constructed for the", "fa-empire", "http://starwars.wikia.com/wiki/Galactic_Empire"),
-        new FooterPun("rebellion", "Join the", "fa-rebel", "http://starwars.wikia.com/wiki/Alliance_to_Restore_the_Republic")
+        new FooterPun("rebellion", "Join the", "fa-rebel", "http://starwars.wikia.com/wiki/Alliance_to_Restore_the_Republic"),
+        new FooterPun("star", "Hey look!", "fa-star fa-spin", null, " It's spinning!")
     ]
 };
