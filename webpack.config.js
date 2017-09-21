7a41fa4df4ae3a73822586a71b8e42caf5e59074
Benjamin Hackl <devel@benjamin-hackl.at>
2017-09-21 12:47:22 +0200

    remove tether; remove bootstrap-loader (bootstrap was loaded twice)

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const webpack = require('webpack');
 
 module.exports = {
-    entry: ["./public/scripts.js", "bootstrap-loader"],
+    entry: ["./public/scripts.js"],
     output: {
         path: path.join(__dirname, 'public/dist'),
         filename: "bundle.js"
@@ -50,11 +50,18 @@ module.exports = {
             $: "jquery",
             jQuery: "jquery",
             "window.jQuery": "jquery",
-            "window.Tether": 'tether',
-            "Tether": 'tether',
-	    "Popper": "popper.js",
-	    "Tooltip": "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
-	    "Util": 'exports-loader?Util!bootstrap/js/dist/util',
+	    Popper: ["popper.js", "default"],
+	    Alert: 'exports-loader?Alert!bootstrap/js/dist/alert',
+            Button: 'exports-loader?Button!bootstrap/js/dist/button',
+            Carousel: 'exports-loader?Carousel!bootstrap/js/dist/carousel',
+            Collapse: 'exports-loader?Collapse!bootstrap/js/dist/collapse',
+            Dropdown: 'exports-loader?Dropdown!bootstrap/js/dist/dropdown',
+            Modal: 'exports-loader?Modal!bootstrap/js/dist/modal',
+            Popover: 'exports-loader?Popover!bootstrap/js/dist/popover',
+            Scrollspy: 'exports-loader?Scrollspy!bootstrap/js/dist/scrollspy',
+            Tab: 'exports-loader?Tab!bootstrap/js/dist/tab',
+            Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
+            Util: 'exports-loader?Util!bootstrap/js/dist/util',
         })
     ]
 };
