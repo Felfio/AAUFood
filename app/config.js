4bbba4ff1bc1cdb80b0f90298384172e2f03308e
Benjamin Hackl <devel@benjamin-hackl.at>
2020-06-08 12:02:27 +0200

    disable winter theme, mensa renovation

--- app/config.js
+++ app/config.js
@@ -5,12 +5,12 @@ module.exports = {
     settings: {
         useRandomFooterPuns: true,
         defaultFooterPun: "heart",
-        winterTheme: true,
+        winterTheme: false,
         nodePort: 3000,
     },
     onBreak: {
         //The dates are only rendered, never used for checking if closed!
-        // mensa: new BreakInfo("Wir befinden uns für den Zeitraum", "28.7", "2.9", "in Sommerpause.", "fa fa-sun"),
+        mensa: new BreakInfo("Wir werden im Zeitraum", "1.6", "27.9", "renoviert und sind in Sommerpause.", "fa fa-sun"),
         // mensa: new BreakInfo("Wir befinden uns für den Zeitraum", "28.7", "2.9", "in Sommerpause."),
     },
     cache: {
