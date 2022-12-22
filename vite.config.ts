886278dcbb7ae6daaafcbc5151e69aee2e6783a8
Fabian Schober <fabian@dirtypaws.studio>
2022-12-23 00:37:01 +0100

    Vercel #11

--- vite.config.ts
+++ vite.config.ts
@@ -20,9 +20,9 @@ export default defineConfig({
   build: {
     chunkSizeWarningLimit: 3000,
     rollupOptions: {
-      input: '/public/scripts.js',
+      input: '/app/public/scripts.js',
       output: {
-        dir: path.join(__dirname, 'public/dist'),
+        dir: path.join(__dirname, 'app/public/dist'),
         assetFileNames: '[name][extname]',
         entryFileNames: 'bundle.js',
         sanitizeFileName(fileName) {
